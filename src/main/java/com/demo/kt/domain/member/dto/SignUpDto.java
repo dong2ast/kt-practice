0e261b9f2f8a3c4606f59760f21742c83431dc6c
dong2ast <kdh01132006@uos.ac.kr>
2025-02-12 21:46:50 +0900

    FEAT: 회원 가입 Validation 구현

--- src/main/java/com/demo/kt/domain/member/dto/SignUpDto.java
+++ src/main/java/com/demo/kt/domain/member/dto/SignUpDto.java
@@ -1,12 +1,29 @@
 package com.demo.kt.domain.member.dto;
 
 import com.demo.kt.domain.member.model.Member;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public record SignUpDto(
+        @Email
+        @NotBlank
+        @Schema(example = "test1234@gmail.com")
         String email,
+        @NotBlank
+        @Pattern(regexp = "^[a-zA-Z가-힣0-9!@#$%^&*()_+=\\-\\[\\]{};':\"\\\\|,.<>\\/?`~]{1,16}$",
+         message = "특수문자, 영문, 한글, 숫자를 포함하여 16자 이내로 입력하세요.")
+        @Schema(example = "@1234qwer")
         String password,
+        @NotBlank
+        @Size(min = 10, max = 11)
+        @Schema(example = "01012345678")
         String phone,
+        @Pattern(regexp = "^[가-힣]{1,16}$", message = "한글만 입력할 수 있으며, 16자 이내여야 합니다.")
+        @Schema(example = "홍길동")
         String name
 ) {
 
--- src/main/java/com/demo/kt/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/demo/kt/global/exception/GlobalExceptionHandler.java
@@ -1,17 +1,43 @@
 package com.demo.kt.global.exception;
 
+import static com.demo.kt.global.enums.ErrorType.REQUEST_VALIDATION_ERROR;
+
 import com.demo.kt.global.common.dto.ApiResponse;
 import com.demo.kt.global.exception.model.CustomException;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    /**
+     * 400 VALIDATION_ERROR
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ApiResponse<?> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+
+        Errors errors = e.getBindingResult();
+        Map<String, String> validateDetails = new HashMap<>();
+
+        for (FieldError error : errors.getFieldErrors()) {
+            String validKeyName = String.format("valid_%s", error.getField());
+            validateDetails.put(validKeyName, error.getDefaultMessage());
+        }
+        return ApiResponse.error(REQUEST_VALIDATION_ERROR, validateDetails);
+    }
 
     /**
      * CUSTOM_ERROR
