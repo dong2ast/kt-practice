46925fad0f1b6de8c649c3692c269b3c7dd420eb
dong2ast <kdh01132006@uos.ac.kr>
2025-02-12 21:45:33 +0900

    FIX: auditing 문제 해결

--- src/main/java/com/demo/kt/KtApplication.java
+++ src/main/java/com/demo/kt/KtApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @EnableAspectJAutoProxy
+@EnableJpaAuditing
 public class KtApplication {
 
     public static void main(String[] args) {
