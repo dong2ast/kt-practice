006e149807af9ba2eb47e4adcdc08ffb69bd6457
dong2ast <kdh01132006@uos.ac.kr>
2025-02-14 11:38:40 +0900

    REFACTOR: 로그인 예시 추가

--- src/main/java/com/demo/kt/domain/member/dto/LoginRequestDto.java
+++ src/main/java/com/demo/kt/domain/member/dto/LoginRequestDto.java
@@ -1,9 +1,12 @@
 package com.demo.kt.domain.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 public record LoginRequestDto(
+        @Schema(example = "test1234@gmail.com")
         String email,
+        @Schema(example = "@1234qwer")
         String password
 ) {
 
