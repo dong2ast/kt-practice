7960528a692886a3cd380c97cfb72f62c18353c3
dong2ast <kdh01132006@naver.com>
2025-03-29 21:52:12 +0900

    FEAT: chatgpt 프롬프팅 코드 작성

--- src/main/java/com/demo/kt/global/ai/GptService.java
+++ src/main/java/com/demo/kt/global/ai/GptService.java
@@ -8,7 +8,7 @@ public class GptService {
     public String generatePrompt(String data) {
         // Diary diary = diaryRepository.findById(memberId) 와 같은 형태로 관찰일지를 불러와 데이터 활용 가능
         String diary = "까미, 5개월, 수컷, 포메라니안\n2025년3월14일, 오늘은 까미가 오른쪽 다리를"
-                + "절뜩거렸다. 병원에 가보니 높은 곳에서 떨어진 충격 때문에 인대에 손상이 생긴 것 같다고 하셨다."
+                + "절뚝거렸다. 병원에 가보니 높은 곳에서 떨어진 충격 때문에 인대에 손상이 생긴 것 같다고 하셨다."
                 + "강아지가 올라오기에는 침대 높이가 너무 높았던 것 같다... 얼렁 까미 전용 계단 만들어줘야지ㅜ";
 
         StringBuilder prompt = new StringBuilder();
